68bf4980828be28bd88a94fbe231c584b70070fc
vet <vetlee@yandex.ru>
2024-08-01 13:34:16 +0300

    shadowsocks_2022 support

--- xtlsapi/api_services/handler/add_client.py
+++ xtlsapi/api_services/handler/add_client.py
@@ -25,22 +25,21 @@ class AddClient(BaseService):
                             'aes_256_gcm':config_pb2.CipherType.AES_256_GCM,
                             'chacha20_poly1305':config_pb2.CipherType.CHACHA20_POLY1305,
                             'xchacha20_poly1305':config_pb2.CipherType.XCHACHA20_POLY1305,
-                            None:config_pb2.CipherType.NONE                            
+                            None:config_pb2.CipherType.NONE
                             }
                 account=config_pb2.Account(password=user_id_or_password,cipher_type=cipher_map.get(cipher,cipher_map[None]))
-            # elif protocol=='shadowsocks_2022':
-                # from xtlsapi.xray_api.proxy.shadowsocks import config_pb2
-                # cipher_map={'aes_128_gcm':config_pb2.CipherType.AES_128_GCM,
-                #             'aes_256_gcm':config_pb2.CipherType.AES_256_GCM,
-                #             'chacha20_poly1305':config_pb2.CipherType.CHACHA20_POLY1305,
-                #             'xchacha20_poly1305':config_pb2.CipherType.XCHACHA20_POLY1305,
-                #             None:config_pb2.CipherType.NONE                            
-                #             }
-                # account=config_pb2.Account(password=user_id_or_password,cipher_type=cipher_map.get(cipher,cipher_map[None])
-                # raise Exception("not implemented ")
+            elif protocol=='shadowsocks_2022':
+                from xtlsapi.xray_api.proxy.shadowsocks_2022 import config_pb2
+                cipher_map={'aes_128_gcm':config_pb2.CipherType.AES_128_GCM,
+                            'aes_256_gcm':config_pb2.CipherType.AES_256_GCM,
+                            'chacha20_poly1305':config_pb2.CipherType.CHACHA20_POLY1305,
+                            'xchacha20_poly1305':config_pb2.CipherType.XCHACHA20_POLY1305,
+                            None:config_pb2.CipherType.NONE
+                            }
+                account=config_pb2.Account(password=user_id_or_password,cipher_type=cipher_map.get(cipher,cipher_map[None]))
             else:
                 raise Exception("not implemented "+protocol )
-            
+
             self.handler_stub.AlterInbound(
                 command_pb2.AlterInboundRequest(
                     tag=inbound_tag,
